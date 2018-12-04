5714fb6bf55d772b8b9474f6ba7a26d9eab588af
NedKhelly <domeybenjamin1@gmail.com>
2018-12-04 13:18:56 -0500

    on it

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Peers Tronix</title>
+  </head>
+  <body>
+
+
+
+    <main>
+      @yield('content')
+    </main
+
+
+  </body>
+</html>
