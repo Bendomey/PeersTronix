c0858a26425efde30527cedf09b8f5b21271e60c
Ben Domey <domeybenjamin1@gmail.com>
2018-12-06 07:29:28 -0500

    done with the contact us  controller

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -9,13 +9,16 @@ class ContactUsController extends Controller
     public function create(Request $req) {
       // save details of  people (contact us)
 
-      $contact = new ConactUs;
-      $contact->full_name = $req->input('full_name');
-      $contact->email = $req->input('email');
-      $contact->service_needed = $req->input('service_needed');
-      $contact->phone = $req->input('phone');
-      $contact->message = $req->input('message');
-      $contact->save();
+      // $contact = new ContactUs;
+      // $contact->full_name = $req->input('full_name');
+      // $contact->email = $req->input('email');
+      // $contact->service_needed = $req->input('service_needed');
+      // $contact->phone = $req->input('phone');
+      // $contact->message = $req->input('message');
+      // $contact->save();
+      // return redirect('contact');
+
+      $contact = ContactUs::create($req->all());
       return redirect('contact');
 
     }
--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -26,6 +26,7 @@
           <div class="container">
             <h2 class="mb-5">Get in Touch</h2>
             <form class="form" action="{{route('save')}}" method="POST">
+              @csrf
               <div class="row mb-4">
                 <div class="col-lg-6 col-md-6 col-sm-12 mb-2">
                   <input type="text" name="full_name" value="" class="form-control form-control-lg" placeholder="Your Name*">
