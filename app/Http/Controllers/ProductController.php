240b6619707f323ec0dc7a191071b3368c14f127
Domey Benjamin <domeybenjamin1@gmail.com>
2019-01-12 20:43:07 +0000

    Hello world

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use App\Product;
+
+class ProductController extends Controller
+{
+
+  public function add_product(Request $request){
+
+    $product = new Product;
+    $product->product_name = $request->product_name;
+    $product->product_price = $request->product_price;
+    $product->product_category = $request->product_category;
+    $product->product_brand = $request->product_brand;
+    $product->product_color = $request->product_color;
+    $product->brief_description = $request->brief_description;
+    $product->description = $request->description;
+    $product->additional_info = $request->additional_info;
+    $product->thumb_picture = $this->image($request->thumb_picture);
+    $product->image_one = $this->image($request->image_one);
+    $product->image_two = $this->image($request->image_two);
+    $product->image_three = $this->image($request->image_three);
+    $product->image_four = $this->image($request->image_four);
+    $product->save();
+    return back()->with('success',"$request->product_name was uploaded successfully");
+  }
+
+
+
+  protected function image($image){
+      $name = md5(microtime());
+      Image::make($image)->save('product_images/'. $name .'.'.$image->getClientOriginalExtension());
+      $image_save = 'product_images/'. $name .'.'.$image->getClientOriginalExtension();
+      return $image_save;
+  }
+
+}
